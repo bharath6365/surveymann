7a2889bb6c92d7e88cbaa7d0b9b9a615eddba502
bharathkumarc <bharath6365@gmail.com>
2017-12-02 10:58:37 +0530

    Fix port

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
-app.listen(5000);
+// Tell passport to use google stratgey
+passport.use(new GoogleStrategy());
+
+app.listen(process.env.PORT || 5000);
